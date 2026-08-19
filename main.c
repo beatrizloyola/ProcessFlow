9d3aac76e471609907487d4fe5c2876684ffee4b
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2026-08-19 16:17:58 -0300

    Tratamento de Erro: Argumentos demais com stderr

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char *argv[]){ // argc é a quantidade de argumentos, argv é um array de string com os argumentos em si
     if (argc > 2){
-        printf("Erro: Argumentos demais!\n");
+        fprintf(stderr, "Erro: Argumentos demais!\n");
         return -1;
     } else if (argc == 2){
         // Modo workflow
