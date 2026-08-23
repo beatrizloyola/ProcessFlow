d062206f69d4d744a7dfaaab871708af93de1836
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2026-08-23 14:21:41 -0300

    Task: campos de file

--- task.h
+++ task.h
@@ -6,6 +6,9 @@ typedef struct Task{
     char programa[256];
     char *argumentos[64];
     int qtd_argumentos;
+    char *input_file;
+    char *output_file;
+    char *append_file;
 } Task;
 
 void criar_task(char *nome, char *programa, char **argumentos, int qtd_argumentos);
